588c4dbb00ab843ccea3f04cc600415126154d85
deftwnes <133358522+deftwnes@users.noreply.github.com>
2024-06-24 08:21:58 -0300

    Add files via upload

---
+++ src/aula11/ativConta/contaCorrente.java
@@ -0,0 +1,31 @@
+package aula11.ativConta;
+
+class contaCorrente extends conta {
+    
+    private float limite;
+
+    public contaCorrente(String titular, String senha, float limite) {
+        super(titular, senha); // Chama o construtor da classe pai (Conta)
+        this.limite = limite;
+    }
+
+    public boolean sacar(float valor) {
+        if (valor > 0 && valor <= (saldo + limite)) {
+            saldo -= valor;
+            System.out.println("Saque de R$ " + valor + " realizado com sucesso. Saldo atual: R$ " + saldo);
+            return true;
+        } else {
+            System.out.println("Saldo insuficiente ou valor de saque inválido.");
+            return false;
+        }
+    }
+
+    public String verificaSaldo() {
+        return "Saldo atual: R$ " + saldo + ", Limite disponível: R$ " + limite;
+    }
+
+
+}
+
+
+    

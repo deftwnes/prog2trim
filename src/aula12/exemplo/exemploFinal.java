d99eea3d254fd2781debd542296116a34c54a257
deftwnes <133358522+deftwnes@users.noreply.github.com>
2024-06-24 10:50:25 -0300

    Add files via upload

---
+++ src/aula12/exemplo/exemploFinal.java
@@ -0,0 +1,10 @@
+package aula12.exemplo;
+
+public class exemploFinal {
+    public final double pi;
+    public static final double PI = 3.141592653589793; 
+    
+    public exemploFinal() {
+    pi = 3.141592653589793;
+    }
+}
---
+++ src/aula12/exemplo/teste.java
@@ -0,0 +1,8 @@
+package aula12.exemplo;
+
+public class teste {
+    public static void main(String[] args){
+        exemploFinal op = new exemploFinal();
+        //op.pi = 25; nao eh possivel trocar o valor de uma variavel final
+    }
+}
